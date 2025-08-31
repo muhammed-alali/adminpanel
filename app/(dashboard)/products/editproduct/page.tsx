9c5526b79574f9541b0f793817df6129f08bdc24
muhammed-alali <elastura444@gmail.com>
2025-09-01 00:03:34 +0300

    updeate type4

--- app/(dashboard)/products/editproduct/page.tsx
+++ app/(dashboard)/products/editproduct/page.tsx
@@ -1,12 +1,16 @@
+"use client";
 import EditProductCom from "@/components/editproduct";
 import TitleHead from "@/components/titleHead";
+import { Suspense } from "react";
 import { CiEdit } from "react-icons/ci";
 
 export default function EditProductPage() {
   return (
     <>
-      <TitleHead title="Edit Product" icon={<CiEdit />} />
-      <EditProductCom />
+      <Suspense fallback={<div>Loading...</div>}>
+        <TitleHead title="Edit Product" icon={<CiEdit />} />
+        <EditProductCom />
+      </Suspense>
     </>
   );
 }
